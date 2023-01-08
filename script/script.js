34f583713230a478b04a61df68f637d22629b0ab
OleksandrKhlopenko <108742474+OleksandrKhlopenko@users.noreply.github.com>
2023-01-08 23:37:51 +0200

    version_3

--- script/script.js
+++ script/script.js
@@ -1,10 +1,10 @@
 "use strict"
- function getData(){ 
+ function getData(){
 fetch('https://jsonplaceholder.typicode.com/posts')
 .then(data => data.json())
-.then(newData => {newData.data.filter ( index => index.id === 1 )} )
+.then(newData => newData.data.filter ( index => index.id === 1 ) )
 //.then(newData=> console.log(newData))
+ }
 
-}
 
 getData();
